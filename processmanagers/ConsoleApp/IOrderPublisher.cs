db190214bc90df23acfb0511c660c53560dd8fad
Michel Grootjans <michel.grootjans@gmail.com>
2017-01-31 22:50:54 +0100

    cleanup

--- processmanagers/ConsoleApp/IOrderPublisher.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace ConsoleApp
-//{
-//    public interface IOrderPublisher
-//    {
-//        void Publish(string topic, Order order);
-//        void Publish<T>(Order order);
-//    }
-//}
