3290ee1c04ca0d78372fd25524c429a18df4f2bb
Hans Loquet <Hans Loquet>
2017-01-31 11:40:42 +0100

    adding repeater

--- processmanagers/ConsoleApp/Cook.cs
+++ processmanagers/ConsoleApp/Cook.cs
@@ -4,16 +4,19 @@ namespace ConsoleApp
 {
     public class Cook : IHandleOrder
     {
+        private readonly string _name;
         private readonly IHandleOrder _handleOrder;
 
-        public Cook(IHandleOrder handleOrder)
+        public Cook(string name, IHandleOrder handleOrder)
         {
+            _name = name;
             _handleOrder = handleOrder;
         }
 
         public void Handle(Order order)
         {
             Thread.Sleep(1000);
+            order.Ingredients.Add("potatoes");
             _handleOrder.Handle(order);
         }
     }
--- processmanagers/ConsoleApp/Program.cs
+++ processmanagers/ConsoleApp/Program.cs
@@ -7,12 +7,33 @@
             var printer = new OrderPrinter();
             var cashier = new Cashier(printer);
             var assistentManager = new AssistentManager(cashier);
-            var cook = new Cook(assistentManager);
-            var waiter = new Waiter(cook);
+            var tom = new Cook("Tom", assistentManager);
+            var basil = new Cook("Basil", assistentManager);
+            var frank = new Cook("Frank", assistentManager);
+            var repeater = new Repeater(tom, basil, frank);
+            var waiter = new Waiter(repeater);
 
             for (var i = 0; i < 10; i++)
             {
                 waiter.PlaceOrder();
+            }
+        }
+    }
+
+    public class Repeater : IHandleOrder
+    {
+        private readonly IHandleOrder[] _handlers;
+
+        public Repeater(params IHandleOrder[] handlers)
+        {
+            _handlers = handlers;
+        }
+
+        public void Handle(Order order)
+        {
+            foreach (var handler in _handlers)
+            {
+                handler.Handle(order);
             }
         }
     }
