bb493e721155af753319e863f7c85478c619abf3
Michel Grootjans <michel.grootjans@gmail.com>
2017-01-31 20:37:01 +0100

    cleanup

--- processmanagers/ConsoleApp/OrderPrinter.cs
+++ processmanagers/ConsoleApp/OrderPrinter.cs
@@ -2,8 +2,8 @@
 {
     public class OrderPrinter : IHandle<OrderPaid>
     {
-        public int Done { get; set; }
-        public int Total { get; set; }
+        public int Done { get; private set; }
+        public int Total { get; private set; }
 
         public void Handle(OrderPaid message)
         {
