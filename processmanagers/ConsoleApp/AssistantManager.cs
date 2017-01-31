88e3b6e4345aab626e55dcd8e962d3f21fac6bef
Michel Grootjans <michel.grootjans@gmail.com>
2017-01-31 12:34:31 +0100

    fixed stupid mistake

---
+++ processmanagers/ConsoleApp/AssistantManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace ConsoleApp
+{
+    public class AssistantManager : IHandleOrder
+    {
+        private readonly IHandleOrder _handleOrder;
+
+
+        public AssistantManager(IHandleOrder handleOrder)
+        {
+            _handleOrder = handleOrder;
+        }
+        public void Handle(Order order)
+        {
+            Thread.Sleep(1000);
+            _handleOrder.Handle(order);
+        }
+    }
+}
