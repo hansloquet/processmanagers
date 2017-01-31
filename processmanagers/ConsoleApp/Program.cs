ac554f86dc881cb80bc993da1f9962d0cd09f024
Michel Grootjans <michel.grootjans@gmail.com>
2017-01-31 10:30:52 +0100

    added a console app

---
+++ processmanagers/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
